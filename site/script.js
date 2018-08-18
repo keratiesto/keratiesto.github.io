3406a144f2ee281b86c10a7f06a29730efb29ebd
keratiesto <41127573+keratiesto@users.noreply.github.com>
2018-08-18 12:02:36 +0300

    0
    0

---
+++ site/script.js
@@ -0,0 +1,13 @@
+menu.onclick = function myFunction() {
+	var x = document.getElementById('myTopnav');
+
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
+}
+
+
+
+section#main>.container>.row>.text-center>ui>li*10>a[href="vk.com"]
